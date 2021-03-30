79f5ede14565d200a31b61e59221bddf310f02ed
awetstone56 <awetstone56@protonmail.ch>
2021-03-29 23:53:57 -0400

    Added sorting and added skeleton of filter

--- movie-tables/src/app/app.module.ts
+++ movie-tables/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MovieTableComponent } from './movie-table/movie-table.component';
 import { MoviesService } from './services/movies.service';
@@ -18,6 +19,7 @@ import { MoviesService } from './services/movies.service';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [MoviesService],
   bootstrap: [AppComponent]
--- movie-tables/src/app/movie-table/movie-table.component.ts
+++ movie-tables/src/app/movie-table/movie-table.component.ts
@@ -9,15 +9,56 @@ import { MoviesService } from '../services/movies.service';
   styleUrls: ['./movie-table.component.scss']
 })
 export class MovieTableComponent implements OnInit {
-  columnHeaders: string[] = [
-    'Title',
-    'Vote Count',
-    'Average Vote',
-    'Popularity',
-    'Poster',
-    'Overview',
-    'Favorite'
+
+  // maybe pull this out into separate class so it doesn't take up so much room in component
+  columnHeaders: any[] = [
+    {
+      title: 'Title', // display name
+      value: 'title', // actual model property name
+      sortable: true, // determines whether this column can be sorted
+      filterable: true, // determines whether this column can be filtered
+    },
+    {
+      title: 'Vote Count',
+      value: 'voteCount',
+      sortable: true,
+      filterable: true,
+    },
+    {
+      title: 'Average Vote',
+      value: 'averageVote',
+      sortable: true,
+      filterable: true,
+    },
+    {
+      title: 'Popularity',
+      value: 'popularity',
+      sortable: true,
+      filterable: true,
+    },
+    {
+      title: 'Poster',
+      value: 'poster',
+      sortable: false,
+      filterable: false,
+    },
+    {
+      title: 'Overview',
+      value: 'overview',
+      sortable: false, // doesn't really make sense for overview to be sorted
+      filterable: true,
+    },
+    {
+      title: 'Favorite',
+      value: 'favorite',
+      sortable: false,
+      filterable: false,
+    },
   ];
+
+  sortByColumn = this.columnHeaders[0];
+  filterByColumn = this.columnHeaders[0];
+  filterText: string = '';
 
   movies: Movie[] = [];
 
@@ -30,7 +71,10 @@ export class MovieTableComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this._moviesService.getMovies().subscribe(movies => this.movies = movies);
+    this._moviesService.getMovies().subscribe(movies => {
+      this.movies = movies;
+      this.onSortChange(this.sortByColumn);
+    });
   }
 
   public deleteMovie(movie: Movie) {
@@ -42,5 +86,36 @@ export class MovieTableComponent implements OnInit {
   public enableVoteEditing(e: any): void {
     if (e)
       this.voteIsEditable = !this.voteIsEditable;
+  }
+
+  public validSortingHeaders(): any[] {
+    return this.columnHeaders.filter(header => header.sortable === true);
+  }
+
+  public validFilteringHeaders(): any[] {
+    return this.columnHeaders.filter(header => header.filterable === true);
+  }
+
+  // sorts in ascending order
+  public onSortChange(e: any) {
+    const column = e.value;
+    this.movies.sort((a, b) => {
+      switch (column) {
+        case 'title':
+          return (a.title < b.title ? -1 : 1);
+        case 'voteCount':
+          return (a.voteCount - b.voteCount);
+        case 'averageVote':
+          return (a.averageVote - b.averageVote);
+        case 'popularity':
+          return (a.popularity - b.popularity);
+        default:
+          return (a.title < b.title ? -1 : 1);
+      }
+    });
+  }
+
+  public onFilterChange(e: any) {
+
   }
 }
